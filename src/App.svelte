186dc88e40e877b6d09c0b89e8cf88ae1fd2b0fa
Guillermo Acosta <guillac124@gmail.com>
2022-11-04 17:12:48 -0300

    svelte course: section 1

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import SignUpPage from './pages/SignUpPage.svelte';
+</script>
+
+<SignUpPage />
---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,13 @@
+import SignUpPage from "./SignUpPage.svelte"
+import { render, screen } from "@testing-library/svelte";
+import '@testing-library/jest-dom';
+
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it('has Sign Up header', () => {
+      render(SignUpPage);
+      const header = screen.getByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
+    });
+  })
+})
